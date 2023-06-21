6f7cd2287af3d819e29425f1dcb7292951fc039c
daffa <draihandika@gmail.com>
2023-06-21 10:17:42 +0700

    finishing

--- lib/onboarding5.dart
+++ lib/onboarding5.dart
@@ -24,106 +24,109 @@ class _onboardingState extends State<onboarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xff163A54),
-        child: Column(children: [
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.only(top: 33, right: 25),
-            child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) {
-                      return Home();
-                    },
-                  ));
-                },
-                child: Text(
-                  'Skip',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontFamily: 'Poppins'),
-                )),
-          ),
-          Container(
-              padding: EdgeInsets.only(left: 52, top: 10),
-              child: Row(
-                children: [
-                  Text('Easy ',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Container(
+          color: Color(0xff163A54),
+          child: Column(children: [
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(top: 10, right: 25),
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) {
+                        return Home();
+                      },
+                    ));
+                  },
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontFamily: 'Poppins'),
+                  )),
+            ),
+            Container(
+                padding: EdgeInsets.only(left: 52, top: 10),
+                child: Row(
+                  children: [
+                    Text('Easy ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35,
+                            color: Colors.white,
+                            fontFamily: 'Poppins')),
+                    Text('Life',
+                        style: TextStyle(
                           fontSize: 35,
                           color: Colors.white,
-                          fontFamily: 'Poppins')),
-                  Text('Life',
-                      style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.white,
-                        fontFamily: 'Poppins-Medium',
-                      ))
-                ],
-              )),
-          Container(
-            padding: EdgeInsets.only(left: 52, top: 10, right: 73, bottom: 20),
-            child: Text(
-                'Lorem Ipsum is simply dummy text of the printing and typesetting industry standard dummy text ever',
-                style: TextStyle(
-                    fontSize: 15, color: Colors.white, fontFamily: 'Roboto')),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                // color: Colors.white,
-                width: 300,
-                child: Image.asset('images/background1.png'),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 50, right: 6),
-                child: Container(
-                  child: Image.asset('images/Rectangle80.png'),
+                          fontFamily: 'Poppins-Medium',
+                        ))
+                  ],
+                )),
+            Container(
+              padding:
+                  EdgeInsets.only(left: 52, top: 10, right: 73, bottom: 20),
+              child: Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry standard dummy text ever',
+                  style: TextStyle(
+                      fontSize: 15, color: Colors.white, fontFamily: 'Roboto')),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  // color: Colors.white,
+                  width: 325,
+                  child: Image.asset('images/background1.png'),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 170),
-                child: Container(
-                  child: Image.asset('images/Ellipse 62.png'),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 6),
+                  child: Container(
+                    child: Image.asset('images/Rectangle80.png'),
+                  ),
                 ),
-              ),
-              // Padding(
-              // padding: EdgeInsets.only(right: 30),
-              // child:
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => onboarding6(),
-                  ));
-                },
-                icon: Icon(Icons.arrow_forward_outlined, size: 30),
-                iconSize: 30,
-                color: Color(0xffDBDD66),
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                padding: EdgeInsets.all(0),
-                constraints: BoxConstraints.tightFor(
-                  width: 70,
-                  height: 53,
+                Padding(
+                  padding: const EdgeInsets.only(right: 170),
+                  child: Container(
+                    child: Image.asset('images/Ellipse 62.png'),
+                  ),
                 ),
-                // optional: tooltip, alignment, and more...
-              )
+                // Padding(
+                // padding: EdgeInsets.only(right: 30),
+                // child:
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => onboarding6(),
+                    ));
+                  },
+                  icon: Icon(Icons.arrow_forward_outlined, size: 30),
+                  iconSize: 30,
+                  color: Color(0xffDBDD66),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  padding: EdgeInsets.all(0),
+                  constraints: BoxConstraints.tightFor(
+                    width: 70,
+                    height: 53,
+                  ),
+                  // optional: tooltip, alignment, and more...
+                )
 
-              // ),
-            ],
-          ),
-        ]),
+                // ),
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
--- lib/onboarding6.dart
+++ lib/onboarding6.dart
@@ -8,102 +8,104 @@ class onboarding6 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xff163A54),
-        child: Column(children: [
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.only(top: 33, right: 25),
-            child: TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'Back',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontFamily: 'Poppins'),
-                )),
-          ),
-          Container(
-              padding: EdgeInsets.only(left: 52, top: 10),
-              child: Row(
-                children: [
-                  Text('Best ',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Container(
+          color: Color(0xff163A54),
+          child: Column(children: [
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(top: 10, right: 25),
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Back',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontFamily: 'Poppins'),
+                  )),
+            ),
+            Container(
+                padding: EdgeInsets.only(left: 52, top: 10),
+                child: Row(
+                  children: [
+                    Text('Best ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35,
+                            color: Colors.white,
+                            fontFamily: 'Poppins')),
+                    Text('Quality',
+                        style: TextStyle(
                           fontSize: 35,
                           color: Colors.white,
-                          fontFamily: 'Poppins')),
-                  Text('Quality',
-                      style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.white,
-                        fontFamily: 'Poppins-Medium',
-                      ))
-                ],
-              )),
-          Container(
-            padding: EdgeInsets.only(left: 52, top: 10, right: 73),
-            child: Text(
-                'Lorem Ipsum is simply dummy text of the printing and typesetting industry standard dummy text ever',
-                style: TextStyle(
-                    fontSize: 15, color: Colors.white, fontFamily: 'Roboto')),
-          ),
-          Container(
-            // color: Colors.white,r
-            child: Image.asset(
-                'images/vecteezy_flat-isometric-concept-3d-illustration-solar-panel-electric_18993723-removebg-preview 1.png'),
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 6, left: 50),
-                child: Container(
-                  child: Image.asset('images/Ellipse 62.png'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 100),
-                child: Container(
-                  child: Image.asset('images/Rectangle80.png'),
-                ),
-              ),
-              // Padding(
-              // padding: EdgeInsets.only(right: 30),
-              // child:
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xffDBDD66), // Warna latar belakang
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(13), // Bentuk border
-                  ),
-                  minimumSize: Size(150, 53), // Ukuran minimum tombol
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Home(),
-                  ));
-                  // Aksi ketika tombol ditekan
-                },
-                child: Text(
-                  'Get Started',
+                          fontFamily: 'Poppins-Medium',
+                        ))
+                  ],
+                )),
+            Container(
+              padding: EdgeInsets.only(left: 52, top: 10, right: 73),
+              child: Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry standard dummy text ever',
                   style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 18,
-                    color: Color(0xff121919),
+                      fontSize: 15, color: Colors.white, fontFamily: 'Roboto')),
+            ),
+            Container(
+              // color: Colors.white,r
+              child: Image.asset(
+                  'images/vecteezy_flat-isometric-concept-3d-illustration-solar-panel-electric_18993723-removebg-preview 1.png'),
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 6, left: 50),
+                  child: Container(
+                    child: Image.asset('images/Ellipse 62.png'),
                   ),
                 ),
-              )
-
-              // ),
-            ],
-          ),
-        ]),
+                Padding(
+                  padding: const EdgeInsets.only(right: 100),
+                  child: Container(
+                    child: Image.asset('images/Rectangle80.png'),
+                  ),
+                ),
+                // Padding(
+                // padding: EdgeInsets.only(right: 30),
+                // child:
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffDBDD66), // Warna latar belakang
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(13), // Bentuk border
+                    ),
+                    minimumSize: Size(150, 53), // Ukuran minimum tombol
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => Home(),
+                    ));
+                    // Aksi ketika tombol ditekan
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 18,
+                      color: Color(0xff121919),
+                    ),
+                  ),
+                )
+      
+                // ),
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
